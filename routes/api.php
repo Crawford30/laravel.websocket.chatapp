928d28d70290e1bc95f3c7390b4f98eea4255b42
Joel Crawford <joelcrawford30@gmail.com>
2023-12-11 23:43:21 +0300

    Connect method controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/sockets/connect', [App\Http\Controllers\SocketsController::class, 'connect'])->name('connect');
